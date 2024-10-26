36958cf09116dbf3166001e6bb72b9ed3b5f1a8c
TonnyleeM <muvunyilee@gmail.com>
2024-10-26 15:46:10 +0000

    complete

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python3
-
+"""Queries the Reddit API and
+prints the titles of the first
+10 hot posts listed for a given
+subreddit.
 """
-Displays the titles of 10 hot posts listed for a subreddit
-"""
-
-from requests import get
-import subprocess
+import requests
 
 
 def top_ten(subreddit):
+    """Prints the titles of the first
+    10 hot posts listed for a given
+    subreddit.
     """
-    The Function that fethces the Reddit API
-    """
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    if response.status_code != 200:
-        print("None")
-        return
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
 
-    try:
-        results = response.json()
-        my_data = results.get('data').get('children')
+    # Set the Query Strings to Request
+    payload = {'limit': '10'}
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
 
-    except Exception:
-        print("None")
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        print('None')
+    else:
+        res_json = res.json()
+
+        if res_json.get('data') and res_json.get('data').get('children'):
+            # Get the 10 hot posts of the subreddit
+            hot_posts = res_json.get('data').get('children')
+
+            # Print each hot post title
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
